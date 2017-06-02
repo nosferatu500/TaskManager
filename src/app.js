ed0b2fca11df268aae3c9e4a09d7d2b84a02cd88
Vitaly Shvetsov <nosferatu2995@mail.ru>
2017-06-02 21:17:01 +0700

    Setup Firebase

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+    apiKey: 'AIzaSyAavV_VYYDZpAD8dIcLy6hZxIH-6bA3wk4',
+    authDomain: 'taskmanager-1c626.firebaseapp.com',
+    databaseURL: 'https://taskmanager-1c626.firebaseio.com',
+    projectId: 'taskmanager-1c626',
+    storageBucket: 'taskmanager-1c626.appspot.com',
+    messagingSenderId: '291795142122'
+  };
+  firebase.initializeApp(config);
+}
+
   render() {
     return (
-
+      
     );
   }
 }
